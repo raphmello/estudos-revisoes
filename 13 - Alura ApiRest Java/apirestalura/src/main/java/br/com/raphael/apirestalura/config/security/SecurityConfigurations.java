e3bf0882281ddbaf5596c428a6633d63b5866171
Raphael de Mello <raph.mello@gmail.com>
2021-09-13 14:41:32 -0300

    Alura - Spring Boot API Rest - Security

---
+++ 13 - Alura ApiRest Java/apirestalura/src/main/java/br/com/raphael/apirestalura/config/security/SecurityConfigurations.java
@@ -0,0 +1,36 @@
+package br.com.raphael.apirestalura.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    //configurações de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    //configurações de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+        .and().formLogin();
+    }
+
+    //configurações de recursos estáticos(js, css, imagens, etc...)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //como estamos desenvolvendo uma api, esses arquivos estarão no frontend
+    }
+}
