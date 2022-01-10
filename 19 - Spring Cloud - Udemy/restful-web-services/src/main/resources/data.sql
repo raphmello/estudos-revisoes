871d421952ce592aa5a84df89acf9f6bcbae6268
Raphael de Mello <raph.mello@gmail.com>
2022-01-10 09:42:18 -0300

    Microservice Initial Commit

--- 19 - Spring Cloud - Udemy/restful-web-services/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user values(1, sysdate(), 'AB');
-INSERT INTO user values(2, sysdate(), 'Kill');
-INSERT INTO user values(3, sysdate(), 'Jam');
-
-INSERT INTO post values(11001, 'My First Post', 1);
-INSERT INTO post values(11002, 'My Second Post', 1);
