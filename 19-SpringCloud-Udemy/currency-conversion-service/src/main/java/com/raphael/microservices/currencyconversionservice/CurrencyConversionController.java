2d97d6be3bac9a9ab61f12fedfe09c589cf9a85c
Raphael de Mello <raph.mello@gmail.com>
2022-01-10 14:50:06 -0300

    OpenFeign API

--- 19-SpringCloud-Udemy/currency-conversion-service/src/main/java/com/raphael/microservices/currencyconversionservice/CurrencyConversionController.java
+++ 19-SpringCloud-Udemy/currency-conversion-service/src/main/java/com/raphael/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.raphael.microservices.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,10 @@ import org.springframework.web.client.RestTemplate;
 import java.math.BigDecimal;
 
 @RestController
-public class CurrencyConversionContrroller {
+public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
@@ -22,6 +26,15 @@ public class CurrencyConversionContrroller {
                         to);
         CurrencyConversion currencyConversion = entity.getBody();
         assert currencyConversion != null;
+        currencyConversion.setQuantity(quantity.multiply(currencyConversion.getConversionMultiple()));
+        return currencyConversion;
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
+                                                          @PathVariable String to,
+                                                          @PathVariable BigDecimal quantity) {
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
         currencyConversion.setQuantity(quantity.multiply(currencyConversion.getConversionMultiple()));
         return currencyConversion;
     }
