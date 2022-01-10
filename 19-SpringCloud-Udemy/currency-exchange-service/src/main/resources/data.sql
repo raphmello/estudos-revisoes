48f19c8ff3793b0c561bd24da77204f000cd614b
Raphael de Mello <raph.mello@gmail.com>
2022-01-10 12:41:54 -0300

    adicionando Spring JPA

---
+++ 19-SpringCloud-Udemy/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into currency_exchange (id, currency_from,currency_to,conversion_multiple,environment) VALUES(10001,'USD','UNR',65,'');
+insert into currency_exchange (id, currency_from,currency_to,conversion_multiple,environment) VALUES(10002,'EUR','UNR',75,'');
+insert into currency_exchange (id, currency_from,currency_to,conversion_multiple,environment) VALUES(10003,'AUD','UNR',25,'');
