3b861a43533eb8b970060c1814132a3ecf9c319e
Raphael de Mello - RDM <ice_rmello@uolinc.com>
2022-04-13 11:06:56 -0300

    add jenkins book

--- 24-Alura Kafka/ecommerce/src/main/java/ecommerce/NewOrderMain.java
+++ 24-Alura Kafka/ecommerce/src/main/java/ecommerce/NewOrderMain.java
@@ -5,21 +5,26 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties())) {
-            String value = "132123,312312,3123123123";
-            ProducerRecord<String, String> recordToSend = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-            producer.send(recordToSend, (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                }
-                System.out.println(data.topic() + ":::" + data.partition() + "/" + data.offset() + "/" + data.timestamp());
-            }).get();
-        }
+        Map<Object, Object> objectObjectMap = Collections.unmodifiableMap(new HashMap<>());
+        objectObjectMap.put("test","teste");
+        //        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties())) {
+//            String value = "132123,312312,3123123123";
+//            ProducerRecord<String, String> recordToSend = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+//            producer.send(recordToSend, (data, ex) -> {
+//                if (ex != null) {
+//                    ex.printStackTrace();
+//                }
+//                System.out.println(data.topic() + ":::" + data.partition() + "/" + data.offset() + "/" + data.timestamp());
+//            }).get();
+//        }
     }
 
     private static Properties properties() {
