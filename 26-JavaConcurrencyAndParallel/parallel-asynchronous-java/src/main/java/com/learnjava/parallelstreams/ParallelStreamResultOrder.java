d28b5ad258e87019d7c7cfca25a270cca5aad4ac
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-06-29 09:42:31 -0300

    comparação de ordenação no processamento paralelo comparando ArrayList e LinkedList

---
+++ 26-JavaConcurrencyAndParallel/parallel-asynchronous-java/src/main/java/com/learnjava/parallelstreams/ParallelStreamResultOrder.java
@@ -0,0 +1,34 @@
+package com.learnjava.parallelstreams;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.LoggerUtil.log;
+
+public class ParallelStreamResultOrder {
+
+    public static List<Integer> listOrder(List<Integer> inputList) {
+        return inputList.parallelStream()
+                .map(integer -> integer * 2)
+                .collect(Collectors.toList());
+    }
+
+    public static Set<Integer> listOrder(Set<Integer> inputList) {
+        return inputList.parallelStream()
+                .map(integer -> integer * 2)
+                .collect(Collectors.toSet());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+        log("inputList : " + inputList);
+        List<Integer> result = listOrder(inputList);
+        log("result : " + result);
+
+        Set<Integer> inputList2 = Set.of(1, 2, 3, 4, 5, 6, 7, 8);
+        log("inputList : " + inputList2);
+        Set<Integer> result2 = listOrder(inputList2);
+        log("result : " + result2);
+    }
+}
