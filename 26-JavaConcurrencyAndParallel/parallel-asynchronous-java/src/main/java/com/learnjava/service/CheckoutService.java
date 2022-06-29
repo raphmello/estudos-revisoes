d0b8852e6251ea9bfccca4dcefbcde878fd22cef
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-06-29 08:57:58 -0300

    cria o exemplo CheckoutService utilizando ParallelStreams e cria seus testes unitários

---
+++ 26-JavaConcurrencyAndParallel/parallel-asynchronous-java/src/main/java/com/learnjava/service/CheckoutService.java
@@ -0,0 +1,40 @@
+package com.learnjava.service;
+
+import com.learnjava.domain.checkout.Cart;
+import com.learnjava.domain.checkout.CartItem;
+import com.learnjava.domain.checkout.CheckoutResponse;
+import com.learnjava.domain.checkout.CheckoutStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.CommonUtil.*;
+
+public class CheckoutService {
+
+    private PriceValidatorService priceValidatorService;
+
+    public CheckoutService(PriceValidatorService priceValidatorService) {
+        this.priceValidatorService = priceValidatorService;
+    }
+
+    public CheckoutResponse checkout(Cart cart) {
+        startTimer();
+
+        List<CartItem> priceValidationList = cart.getCartItemList()
+                .parallelStream()
+                .peek(cartItem -> {
+                    boolean isPriceInvalid = priceValidatorService.isCartItemInvalid(cartItem);
+                    cartItem.setExpired(isPriceInvalid);
+                })
+                .filter(CartItem::isExpired)
+                .collect(Collectors.toList());
+
+        if (priceValidationList.size() > 0) {
+            return new CheckoutResponse(CheckoutStatus.FAILURE, priceValidationList);
+        }
+
+        timeTaken();
+        return new CheckoutResponse(CheckoutStatus.SUCCESS);
+    }
+}
---
+++ 26-JavaConcurrencyAndParallel/parallel-asynchronous-java/src/test/java/com/learnjava/service/CheckoutServiceTest.java
@@ -0,0 +1,33 @@
+package com.learnjava.service;
+
+import com.learnjava.domain.checkout.Cart;
+import com.learnjava.domain.checkout.CheckoutResponse;
+import com.learnjava.domain.checkout.CheckoutStatus;
+import com.learnjava.util.DataSet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.stopWatch;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckoutServiceTest {
+
+    CheckoutService checkoutService = new CheckoutService(new PriceValidatorService());
+
+    @Test
+    void numberOfCores() {
+        int i = Runtime.getRuntime().availableProcessors();
+        System.out.println(i);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {8, 12, 16, 22})
+    void checkout(int nOfItems) {
+        Cart cart = DataSet.createCart(nOfItems);
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+        assertEquals(CheckoutStatus.FAILURE, checkoutResponse.getCheckoutStatus());
+        stopWatch.reset();
+    }
+}
