faa229b74f696e13c530097a0392c642c9f5758f
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-06-30 10:34:58 -0300

    exemplo de ajustar o ForkJoinPool

--- 26-JavaConcurrencyAndParallel/parallel-asynchronous-java/src/test/java/com/learnjava/service/CheckoutServiceTest.java
+++ 26-JavaConcurrencyAndParallel/parallel-asynchronous-java/src/test/java/com/learnjava/service/CheckoutServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.concurrent.ForkJoinPool;
+
 import static com.learnjava.util.CommonUtil.startTimer;
 import static com.learnjava.util.CommonUtil.stopWatch;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,12 +24,26 @@ class CheckoutServiceTest {
         System.out.println(i);
     }
 
+    @Test
+    void parallelism() {
+        System.out.println(ForkJoinPool.getCommonPoolParallelism());
+    }
+
     @ParameterizedTest
-    @ValueSource(ints = {6})
+    @ValueSource(ints = {8, 15,16})
     void checkout(int nOfItems) {
         Cart cart = DataSet.createCart(nOfItems);
         CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
         assertEquals(CheckoutStatus.SUCCESS, checkoutResponse.getCheckoutStatus());
         assertTrue(checkoutResponse.getFinalRate() > 0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100})
+    void modify_parallelism(int nOfItems) {
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "100");
+        Cart cart = DataSet.createCart(nOfItems);
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+        assertEquals(CheckoutStatus.FAILURE, checkoutResponse.getCheckoutStatus());
     }
 }
