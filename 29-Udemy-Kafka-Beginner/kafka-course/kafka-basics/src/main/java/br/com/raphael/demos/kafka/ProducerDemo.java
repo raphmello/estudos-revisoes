dad8f086e9e1bd4d3b5ea00d0fe20e0663ee99e2
Raphael de Mello - RDM <raph.mello@gmail.com>
2022-10-18 08:50:49 -0300

    Kafka - Creating Java Producer

---
+++ 29-Udemy-Kafka-Beginner/kafka-course/kafka-basics/src/main/java/br/com/raphael/demos/kafka/ProducerDemo.java
@@ -0,0 +1,33 @@
+package br.com.raphael.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("demo_java", "hello world");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
