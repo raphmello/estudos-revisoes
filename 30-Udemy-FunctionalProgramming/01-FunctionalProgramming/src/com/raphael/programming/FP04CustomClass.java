e7fc7735587b96c903c6d0d5a15a24e1fb16ec62
Raphael de Mello - RDM <raph.mello@gmail.com>
2023-02-09 09:00:23 -0300

    examples of methods -> skip, limit, takeWhile, sum, max, min, count, groupingBy

--- 30-Udemy-FunctionalProgramming/01-FunctionalProgramming/src/com/raphael/programming/FP04CustomClass.java
+++ 30-Udemy-FunctionalProgramming/01-FunctionalProgramming/src/com/raphael/programming/FP04CustomClass.java
@@ -1,7 +1,9 @@
 package com.raphael.programming;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 class Course {
     private String name;
@@ -82,5 +84,90 @@ public class FP04CustomClass {
                 .anyMatch(reviewScoreGreaterThen95);
         System.out.println(anyMatch);
 
+        System.out.println("----------------------------------------");
+        Comparator<Course> comparingByNumberOfStudentsAsc = Comparator.comparing(Course::getNoOfStudents);
+        Comparator<Course> comparingByNumberOfStudentsDesc = Comparator.comparing(Course::getNoOfStudents).reversed();
+
+        List<Course> sortedCourses = courses.stream()
+                .sorted(comparingByNumberOfStudentsAsc).collect(Collectors.toList());
+        System.out.println(sortedCourses);
+        List<Course> reversedSortedCourses = courses.stream()
+                .sorted(comparingByNumberOfStudentsDesc).collect(Collectors.toList());
+        System.out.println(reversedSortedCourses);
+
+        System.out.println("----------------------------------------");
+        Comparator<Course> comparingByNumberOfStudentsAndNoOfReviews = Comparator.comparing(Course::getNoOfStudents)
+                .thenComparing(Course::getReviewScore).reversed();
+        List<Course> sortedCoursesCompound = courses.stream()
+                .sorted(comparingByNumberOfStudentsAndNoOfReviews).collect(Collectors.toList());
+        System.out.println(sortedCoursesCompound);
+
+        List<Course> sortedCoursesCompoundLimited = courses.stream()
+                .sorted(comparingByNumberOfStudentsAndNoOfReviews)
+                .limit(5)
+                .collect(Collectors.toList());
+        System.out.println(sortedCoursesCompoundLimited);
+
+        List<Course> sortedCoursesCompoundSkip = courses.stream()
+                .sorted(comparingByNumberOfStudentsAndNoOfReviews)
+                .skip(3)
+                .collect(Collectors.toList());
+        System.out.println(sortedCoursesCompoundSkip);
+
+        List<Course> sortedCoursesCompoundTakeWhile = courses.stream()
+                .sorted(comparingByNumberOfStudentsAndNoOfReviews)
+                .takeWhile(course -> course.getReviewScore() >= 95)
+                .collect(Collectors.toList());
+        System.out.println(sortedCoursesCompoundTakeWhile);
+
+        List<Course> sortedCoursesCompoundDropWhile = courses.stream()
+                .sorted(comparingByNumberOfStudentsAndNoOfReviews)
+                .dropWhile(course -> course.getReviewScore() >= 95)
+                .collect(Collectors.toList());
+        System.out.println(sortedCoursesCompoundDropWhile);
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .max(comparingByNumberOfStudentsAndNoOfReviews));
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .min(comparingByNumberOfStudentsAndNoOfReviews));
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .filter(reviewScoreGreaterThen95)
+                .mapToInt(Course::getNoOfStudents)
+                .sum());
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .filter(reviewScoreGreaterThen95)
+                .mapToInt(Course::getNoOfStudents)
+                .average());
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .filter(reviewScoreGreaterThen95)
+                .mapToInt(Course::getNoOfStudents)
+                .count());
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .filter(reviewScoreGreaterThen95)
+                .mapToInt(Course::getNoOfStudents)
+                .max());
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .collect(Collectors.groupingBy(Course::getCategory, Collectors.counting())));
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .collect(Collectors.groupingBy(Course::getCategory, Collectors.maxBy(Comparator.comparing(Course::getReviewScore)))));
+
+        System.out.println("------------------------------------------------");
+        System.out.println(courses.stream()
+                .collect(Collectors.groupingBy(Course::getCategory, Collectors.mapping(Course::getName, Collectors.toList()))));
     }
 }
