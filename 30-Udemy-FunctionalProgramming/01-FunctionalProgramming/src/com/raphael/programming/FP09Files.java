70eca17705af6acd1cba047885f06f4bd9716961
Raphael de Mello - RDM <raph.mello@gmail.com>
2023-02-15 09:21:53 -0300

    Reading files and directories with functional programming

---
+++ 30-Udemy-FunctionalProgramming/01-FunctionalProgramming/src/com/raphael/programming/FP09Files.java
@@ -0,0 +1,22 @@
+package com.raphael.programming;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class FP09Files {
+    public static void main(String[] args) throws IOException {
+        Files.lines(Paths.get("text-file.txt"))
+                .map(str -> str.split(" "))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+
+        System.out.println("---------------------------------------------");
+        Files.list(Paths.get("."))
+                .filter(Files::isDirectory)
+                .forEach(System.out::println);
+    }
+}
